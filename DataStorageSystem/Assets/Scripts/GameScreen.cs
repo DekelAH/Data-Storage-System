477653520a3c875b016b495afd48377010f21002
DekelA <dekel16791@gmail.com>
2022-11-14 19:22:25 +0200

    Update

---
+++ DataStorageSystem/Assets/Scripts/GameScreen.cs
@@ -0,0 +1,47 @@
+﻿#region Copy
+// This file is © 2022 Rumyancev Pavel <paulrumyancev@gmail.com>
+// Vector. School of Game development
+#endregion
+using UnityEngine;
+
+namespace VectorSchool
+{
+    public class GameScreen : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField]
+        private int _coinsToAdd = 30;
+        
+        [SerializeField]
+        private int _coinsToTake = 30;
+
+        [SerializeField]
+        private int _gemsToAdd = 1;
+
+        [SerializeField]
+        private int _gemsToTake = 1;
+        
+        #endregion
+        
+        #region Methods
+
+        public void OnAddCoinsButtonClick()
+        {
+        }
+
+        public void OnTakeCoinsButtonClick()
+        {
+        }
+
+        public void OnAddGemsButtonClick()
+        {
+        }
+
+        public void OnTakeGemsButtonClick()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ DataStorageSystem/Assets/Scripts/PlayerDataView.cs
@@ -0,0 +1,31 @@
+﻿#region Copy
+// This file is © 2022 Rumyancev Pavel <paulrumyancev@gmail.com>
+// Vector. School of Game development
+#endregion
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace VectorSchool
+{
+    public class PlayerDataView : MonoBehaviour
+    {
+        #region Editor
+
+        [SerializeField]
+        private Text _coinsBalance;
+
+        [SerializeField]
+        private Text _gemsBalance;
+        
+        #endregion
+        
+        #region Methods
+
+        private void Start()
+        {
+        }
+        
+        #endregion
+    }
+}
